fb1909249bac433327e9bc82b5547eff7cf90489
blait455 <blait455@gmail.com>
2020-05-25 08:08:51 -0400

    admin dashboard view

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copyDirectory('resources/backend', 'public/backend');
+mix.copyDirectory('resources/frontend', 'public/frontend');
+
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
