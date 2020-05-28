db5fe7ad9030fbca589ef414ee6eb02f109a5721
blait455 <blait455@gmail.com>
2020-05-27 22:01:09 -0400

    product images for product section

--- routes/admin.php
+++ routes/admin.php
@@ -59,7 +59,9 @@ Route::group(['prefix'  =>  'admin'], function () {
             Route::post('/store', 'Admin\ProductController@store')->name('admin.products.store');
             Route::get('/edit/{id}', 'Admin\ProductController@edit')->name('admin.products.edit');
             Route::post('/update', 'Admin\ProductController@update')->name('admin.products.update');
-         
+
+            Route::post('images/upload', 'Admin\ProductImageController@upload')->name('admin.products.images.upload');
+            Route::get('images/{id}/delete', 'Admin\ProductImageController@delete')->name('admin.products.images.delete');
          });
     });
 
