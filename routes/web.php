11a674385e487b2d6a8973ee170dc623334dadad
blait455 <blait455@gmail.com>
2020-05-29 11:49:25 -0400

    shopping cart

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
 
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
-
 Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
+
+Route::get('/cart', 'Site\CartController@getCart')->name('checkout.cart');
+Route::get('/cart/item/{id}/remove', 'Site\CartController@removeItem')->name('checkout.cart.remove');
+Route::get('/cart/clear', 'Site\CartController@clearCart')->name('checkout.cart.clear');
 
 require 'admin.php';
