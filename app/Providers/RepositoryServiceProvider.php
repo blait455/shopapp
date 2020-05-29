84bc6d73366b1f6a4ca6a74adbf6018dcc037130
blait455 <blait455@gmail.com>
2020-05-29 16:51:55 -0400

    checkout

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Contracts\CategoryContract;
+use Illuminate\Support\ServiceProvider;
 use App\Repositories\CategoryRepository;
 use App\Contracts\AttributeContract;
 use App\Repositories\AttributeRepository;
@@ -11,6 +11,8 @@ use App\Contracts\BrandContract;
 use App\Repositories\BrandRepository;
 use App\Contracts\ProductContract;
 use App\Repositories\ProductRepository;
+use App\Contracts\OrderContract;
+use App\Repositories\OrderRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
         AttributeContract::class        =>          AttributeRepository::class,
         BrandContract::class            =>          BrandRepository::class,
         ProductContract::class          =>          ProductRepository::class,
+        OrderContract::class            =>          OrderRepository::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +12,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'site.pages.homepage');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
-
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
-Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
 
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
 Route::get('/cart', 'Site\CartController@getCart')->name('checkout.cart');
 Route::get('/cart/item/{id}/remove', 'Site\CartController@removeItem')->name('checkout.cart.remove');
 Route::get('/cart/clear', 'Site\CartController@clearCart')->name('checkout.cart.clear');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/checkout', 'Site\CheckoutController@getCheckout')->name('checkout.index');
+    Route::post('/checkout/order', 'Site\CheckoutController@placeOrder')->name('checkout.place.order');
+
+    Route::get('checkout/payment/complete', 'Site\CheckoutController@complete')->name('checkout.payment.complete');
+
+    Route::get('account/orders', 'Site\AccountController@getOrders')->name('account.orders');
+});
+
+Auth::routes();
 require 'admin.php';
