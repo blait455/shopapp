d3023eb2146ca5566dc2b489094e42bb6993b4b5
blait455 <blait455@gmail.com>
2020-05-29 07:50:29 -0400

    categories navigation

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
+
 require 'admin.php';
